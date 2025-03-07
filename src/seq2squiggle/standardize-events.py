c367e269c74c0cf0286ed76b951064999f3a765c
denisbeslic <denis.beslic@outlook.com>
2025-03-07 18:10:37 +0100

    Remove filtering of events_stdv above 10.0

--- src/seq2squiggle/standardize-events.py
+++ src/seq2squiggle/standardize-events.py
@@ -18,7 +18,7 @@ def calculate_global_mean(input_file, chunk_size):
         df_chunk = pl.concat(batches)
         df_chunk = df_chunk.with_columns(pl.col("event_stdv").cast(pl.Float64))
         # Filter rows where event_stdv is less than or equal to 10.0
-        df_chunk = df_chunk.filter(pl.col("event_stdv") <= 10.0)
+        # df_chunk = df_chunk.filter(pl.col("event_stdv") <= 10.0)
 
         total_sum += df_chunk["event_stdv"].sum()
         total_count += df_chunk.shape[0]
